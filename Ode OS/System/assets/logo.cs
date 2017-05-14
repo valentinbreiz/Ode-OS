e681dd219af95628d3ecc5a6818d17cd6faa218a
Valentin Charbonnier <valentinbreiz@gmail.com>
2017-05-14 17:19:37 +0200

    Minor modifications... #1

--- Ode OS/System/assets/logo.cs
+++ Ode OS/System/assets/logo.cs
@@ -12,23 +12,24 @@ namespace Ode_OS.System.assets
     {
         internal static void OdeOSlogo(string version)
         {
+
             Console.WriteLine(" ");
-            Console.WriteLine("|------------------------------------------------------------------------------|");
+            Console.WriteLine("*------------------------------------------------------------------------------*");
             Console.ForegroundColor = ConsoleColor.DarkRed;
-            Console.WriteLine(@"   ____  _____  ______");
+            Console.WriteLine(@"  .88888.        dP");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(@"  / __ \|  __ \|  ____|");
+            Console.WriteLine(@" d8'   `8b       88");
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(@" | |  | | |  | | |__   ");
+            Console.WriteLine(@" 88     88 .d888b88 .d8888b.");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(@" | |  | | |  | |  __|  ");
+            Console.WriteLine(@" 88     88 88'  `88 88ooood8");
             Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine(@" | |__| | |__| | |____                                  version " + version);
+            Console.WriteLine(@" Y8.   .8P 88.  .88 88.                                  version " + version);
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine(@"  \____/|_____/|______|  operating system               cree par valentinbreiz");
+            Console.WriteLine(@"  `8888P'  `88888P8 `88888P' operating system           cree par valentinbreiz");
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(" ");
-            Console.WriteLine("|------------------------------------------------------------------------------|");
+            Console.WriteLine("*------------------------------------------------------------------------------*");
             Console.WriteLine(" ");
         }
     }
